777d21410fee6e1f021e802b93da4f2868ead323
ayleva <ayllayeva@gmail.com>
2024-10-31 02:02:49 +0400

    initial commit

---
+++ jsontask1/jsontask1/Program.cs
@@ -0,0 +1,118 @@
+﻿using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace jsontask1
+{
+  
+    internal class Program
+    {
+        public static string Pathname;
+       
+      
+
+
+        static void Main(string[] args)
+        {
+
+            Pathname = Path.GetFullPath(Path.Combine("..", "..", "..", "Files", "names.json"));
+
+
+            List<string> Names = new List<string> { "Samurai", "Ayla", "Nurane", "Kubra", "Mehin" };
+
+            //Addname("Nezrin");
+            //Addname("Zerife");
+            //Console.WriteLine(Search("Nezrin", n => n == "FalseMan"));
+
+            Delete(4);
+
+
+            string result = JsonConvert.SerializeObject(Names);
+
+            using (StreamWriter sw = new StreamWriter(Pathname, true))
+            {
+                sw.WriteLine(result);
+            }
+
+
+
+
+        }
+     
+
+
+        static void Addname(string name)
+        {
+
+            List<string> names = new List<string>();
+            string result;
+            using (StreamReader sr = new StreamReader(Pathname))
+            {
+                result = sr.ReadToEnd();
+            }
+
+            names = JsonConvert.DeserializeObject<List<string>>(result);
+
+            names = JsonConvert.DeserializeObject<List<string>>(result);
+
+            names.Add(name);
+
+            string result2 = JsonConvert.SerializeObject(names);
+
+            using (StreamWriter sw = new StreamWriter(Pathname))
+            {
+                sw.Write(result2);
+            }
+
+        }
+
+        static bool Search(string name,Predicate<string> func)
+        {
+            List<string>names=new List<string>();
+            string result;
+            using(StreamReader sr=new StreamReader(Pathname))
+            {
+                result=sr.ReadToEnd();
+            }
+
+           names=JsonConvert.DeserializeObject<List<string>>(result);
+
+         for(int i=0;i<names.Count; i++)
+            {
+                if (func(names[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        static void Delete(int index)
+        {
+            List<string> names = new List<string>();
+            string result;
+            using (StreamReader sr = new StreamReader(Pathname))
+            {
+                result = sr.ReadToEnd();
+            }
+
+            names = JsonConvert.DeserializeObject<List<string>>(result);
+
+            for(int i = 0; i < names.Count; i++)
+            {
+                if (i == index)
+                {
+                    names.Remove(names[i]);
+                }
+            }
+          
+
+           string result2 = JsonConvert.SerializeObject(names);
+            using(StreamWriter sw = new StreamWriter(Pathname))
+            {
+                sw.Write(result2);
+            }
+        }
+    }
+}
